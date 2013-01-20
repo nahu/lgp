fecd196e0b4e79fbd6bb3f977d178a2ad4667313
Vanessa Cañete <vanessa.can.89@gmail.com>
2013-01-20 17:18:37 -0300

    Carpeta para el código de programación lineal en Python.
    Reader.py : Clase para pasar los datos del archivo a una lista de
    listas. (Matriz).
    get_matrix_from_file(self, filename): método que lee  el archivo y
    retorna número de columnas, filas y la matriz de datos.

---
+++ LgpPython/Reader.py
@@ -0,0 +1,29 @@
+
+class Reader:
+
+    def get_matrix_from_file(self, filename):
+        _file = open(filename,'r')
+        #Formato del archivo: Linea 1: columnas, linea 2: filas, Linea 3 en adelante: datos        n = _file.readline()
+        n = _file.readline()
+        T = _file.readline()
+        matrix = []
+        for i in range(int(T)): #T filas
+            line=_file.readline().split(";");
+            matrix.append(line[0:int(n)]) #n columnas
+        _file.close()
+        #Se retorna: cantidad de columnas, cantidad de filas, matriz de datos
+        return(int(n), int(T), matrix) 
+
+if __name__ == "__main__":
+    r = Reader()
+    """La tupla retornada tiene: (columnas, filas, matriz)"""
+    tuple = r.get_matrix_from_file("Datos60.txt")
+    
+    print "Transformadores> "
+    print tuple[0]
+    print "Muestras> "
+    print tuple[1]
+    print "Datos>"
+    for i in tuple[2]:
+        print i
+    
