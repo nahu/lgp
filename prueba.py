1fb3eeef5a3df2ef8d137f364b4e15ae0f365d5e
Nahuel <nahuel.11990@gmail.com>
2013-01-22 23:46:18 -0300

    Implementación de función de evaluación de fitness para un individuo

--- prueba.py
+++ prueba.py
@@ -10,9 +10,13 @@ Módulo que define las funciones asociadas al algoritmo LGP
 """
 from sets import Set
 
+import math
+import copy
+
 config = "1010101010101"
 n = len(config)
 k = 0
+index_to_predict = 1
 
 for i in config:
     if (i == '1'):
@@ -21,9 +25,9 @@ for i in config:
 #REGISTROS
 """
 r[0] registro de salida
-r[1] .. r[k] registros de entrada constantes
-r[k+1] .. r[2*k] registros aleatorios constantes
-r[2*k + 1] .. r[4*k] registros variables inicializados a 1
+r[1] .. r[k] registros aleatorios constantes
+r[k + 1] .. r[3*k] registros variables inicializados a 1
+r[3*k + 1] .. r[4*k] registros de entrada constantes
 """
 
 num_registers = 5*k + 1
@@ -32,6 +36,7 @@ num_var_register = 2*k
 num_const_in_registers = k
 num_out_registers = 1
 num_const_random_registers = k
+register_offset = 3*k + 1
 
 #INSTRUCCIONES
 """
@@ -63,33 +68,33 @@ validation_lines = 96
 """
 para armar las instrucciones usando la función string.format()
 según la operación se usa:
-operators[num_operation].format(id_individual, reg_dest, reg_op1 - num_conts_registers, reg_op2, t)
+operations[num_operation].format(id_individual, reg_dest, reg_op1, reg_op2 - reg_offset)
 para obtener el string con la instrucción
 
 en operaciones con dos operandos, el primero siempre es variable
 y el segundo puede ser constante 
 o variable con cierta probabilidad (p_reg_op2)
 
-para registros constantes en el segundo operador usar operators[num_operation<<4]
+para registros constantes en el segundo operador usar operations[num_operation<<4]
 """
-operators = {   1   : 'r_all[{0}][{1}]=r_all[{0}][{2}]+r_all[{0}][{3}]',
-                16  : 'r_all[{0}][{1}]=r_all[{0}][{2}]+r_const[{4}][{3}]',
-                2   : 'r_all[{0}][{1}]=r_all[{0}][{2}]-r_all[{0}][{3}]',
-                32  : 'r_all[{0}][{1}]=r_all[{0}][{2}]-r_const[{4}][{3}]',
-                3   : 'r_all[{0}][{1}]=r_all[{0}][{2}]*r_all[{0}][{3}]',
-                48  : 'r_all[{0}][{1}]=r_all[{0}][{2}]*r_const[{4}][{3}]',
-                4   : 'r_all[{0}][{1}]=r_all[{0}][{2}]/r_all[{0}][{3}]',
-                64  : 'r_all[{0}][{1}]=r_all[{0}][{2}]/r_const[{4}][{3}]',
-                5   : 'r_all[{0}][{1}]=r_all[{0}][{2}]**r_all[{0}][{3}]',
-                80  : 'r_all[{0}][{1}]=r_all[{0}][{2}]**r_const[{4}][{3}]',
-                6   : 'r_all[{0}][{1}]=math.log10(r_all[{0}][{3}])',
-                96  : 'r_all[{0}][{1}]=math.log10(r_const[{4}][{3}])',
-                7   : 'r_all[{0}][{1}]=math.sqrt(r_all[{0}][{3}])',
-                112 : 'r_all[{0}][{1}]=math.sqrt(r_const[{4}][{3}])',
-                8   : 'r_all[{0}][{1}]=math.sen(r_all[{0}][{3}])',
-                128 : 'r_all[{0}][{1}]=math.sen(r_const[{4}][{3}])',
-                9   : 'r_all[{0}][{1}]=math.cos(r_all[{0}][{3}])',
-                144 : 'r_all[{0}][{1}]=math.cos(r_const[{4}][{3}])'
+operations = {  1   : 'r_all[{0}]=r_all[{1}]+r_all[{2}]',
+                16  : 'r_all[{0}]=r_all[{1}]+in_t[{2}]',
+                2   : 'r_all[{0}]=r_all[{1}]-r_all[{2}]',
+                32  : 'r_all[{0}]=r_all[{1}]-in_t[{2}]',
+                3   : 'r_all[{0}]=r_all[{1}]*r_all[{2}]',
+                48  : 'r_all[{0}]=r_all[{1}]*in_t[{2}]',
+                4   : 'r_all[{0}]=r_all[{1}]/r_all[{2}]',
+                64  : 'r_all[{0}]=r_all[{1}]/in_t[{2}]',
+                5   : 'r_all[{0}]=r_all[{1}]**r_all[{2}]',
+                80  : 'r_all[{0}]=r_all[{1}]**in_t[{2}]',
+                6   : 'r_all[{0}]=math.log10(r_all[{2}])',
+                96  : 'r_all[{0}]=math.log10(in_t[{2}])',
+                7   : 'r_all[{0}]=math.sqrt(r_all[{2}])',
+                112 : 'r_all[{0}]=math.sqrt(in_t[{2}])',
+                8   : 'r_all[{0}]=math.sen(r_all[{2}])',
+                128 : 'r_all[{0}]=math.sen(in_t[{2}])',
+                9   : 'r_all[{0}]=math.cos(r_all[{2}])',
+                144 : 'r_all[{0}]=math.cos(in_t[{2}])'
                 }
 
 
@@ -143,7 +148,7 @@ def mark_effective_instructions(instructions):
     intructions[i][4] = efectiva o no efectiva
     """
     reg_eff = set([0])
-    
+    eff_i = []
     for i in reversed(instructions):
         if (i[1] in reg_eff):
             # los operadores unarios tiene identificador del 6 al 9
@@ -153,10 +158,33 @@ def mark_effective_instructions(instructions):
                 reg_eff.add(i[2])
             
             reg_eff.add(i[3])
-            i[4] = True
+            #i[4] = True
+            eff_i.append(i)
+            
+    return eff_i.reverse()
 
 
-def eval_fitness():
-    pass
+"""
+se debe llamar com eval_fitness(individual.genomeList)
+"""
+def eval_fitness(individual):
+    eff_instructions = mark_effective_instructions(individual.genomeList)
+    
+    program = ""
+    for i in eff_instructions:
+        if (i[3] >= register_offset):
+            program += operations[i[0]<<4].format(i[1], i[2], i[3] - register_offset)
+        else:
+            program += operations[i[0]].format(i[1], i[2], i[3])
+    
+    #in_t tiene las mediciones en el isntante t
+    error_a_quad = 0
+    for t in range(0, training_lines):
+        in_t = r_const[t]
+        r_all = copy.copy(individual.r_all)
+        exec program
+        error_a_quad += (r_all[0] - X[t][index_to_predict]) ** 2
+        
+    fitness = 1/error_a_quad
 
 
