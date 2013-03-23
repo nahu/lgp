28fa3b01892ca62d3e149494499da1d4f4f664d2
Vanessa <vanessa.can.89@gmail.com>
2013-03-23 18:05:46 -0300

    Lectura del archivo

--- LgpCpp/src/main.cpp
+++ LgpCpp/src/main.cpp
@@ -9,16 +9,35 @@
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
+#include "parameters.h"
+#include <string.h>
+//#include "util.h"
 
-#include "util.h"
+#include <iostream>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "file_util.h"
 using namespace std;
 
 int main() {
 	srand((unsigned)time(0));
+	//Se crea la matriz -- ver si corresponde crear en parameters.
+	double ** data = new double*[LINES];
+	// Se crea una linea por muestra.
+	int current_sample = 0;
+	for (current_sample = 0; current_sample < LINES; current_sample++ )
+	{
+		data[current_sample] = new double[N];
+	}
+	//se carga la matriz desde el archivo
+	data = get_matrix_from_file();
+	//se imprime la matriz
+	imprimir_matriz(data);
 
-
-
-	cout << "!!!Hello World!!!" << endl;
 	return 0;
 }
