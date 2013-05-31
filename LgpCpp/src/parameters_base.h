231ae7ecec270e0437532c7e6baae2fd287586bb
Vanessa <vanessa.can.89@gmail.com>
2013-05-30 21:52:21 -0400

    ultimos cambios

--- LgpCpp/src/parameters_base.h
+++ LgpCpp/src/parameters_base.h
@@ -8,6 +8,7 @@
 #define _USE_MATH_DEFINES
 
 #define FILE_NAME "./data/Datos8.txt"
+#define FILE_ANALISIS "./analisis/analisis.csv"
 //#define FILE_NAME_DR "./data/datos_reducidos.txt"
 
 //Solo faltan 5 transformadores. [1,7,15,23,39]
